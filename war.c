ba0b7b596909fc46f5ff14f8e331ea2366755f5c
Dev123we <160235007+Dev123we@users.noreply.github.com>
2026-04-11 22:51:03 +0000

    Implementação da parte 1 do jogo War.

--- war.c
+++ war.c
@@ -11,16 +11,28 @@
 // - Utilizar passagem por referência (ponteiros) para modificar dados e
 //   passagem por valor/referência constante (const) para apenas ler.
 // - Foco em: Design de software, modularização, const correctness, lógica de jogo.
+
+// Esse projeto se trata da PARTE 1 do famoso jogo chamado WAR, aqui utilizamos e
+//   e reforçamos os conceitos da metodologia struct para fazer com que nosso código
+//   fique mais organizado e legível. 
 //
 // ============================================================================
 
 // Inclusão das bibliotecas padrão necessárias para entrada/saída, alocação de memória, manipulação de strings e tempo.
+#include <stdio.h>
+#include <string.h>
 
 // --- Constantes Globais ---
 // Definem valores fixos para o número de territórios, missões e tamanho máximo de strings, facilitando a manutenção.
+#define TERR_MAX 5 //Necessitei de apenas uma constante.
 
 // --- Estrutura de Dados ---
 // Define a estrutura para um território, contendo seu nome, a cor do exército que o domina e o número de tropas.
+typedef struct Territorio{ // Esta é nossa struct com o apelido territorio, utilizei o typdef
+	char nome[30];         // para garantir um código mais legível.
+	char cor[10];
+	int tropas;
+}Territorio;
 
 // --- Protótipos das Funções ---
 // Declarações antecipadas de todas as funções que serão usadas no programa, organizadas por categoria.
@@ -28,6 +40,10 @@
 // Funções de interface com o usuário:
 // Funções de lógica principal do jogo:
 // Função utilitária:
+void LimparBufferEntrada(){ // Função limpar buffer de entrada.
+	int x; 
+	while ((x = getchar()) != '\n' && x != EOF);
+}
 
 // --- Função Principal (main) ---
 // Função principal que orquestra o fluxo do jogo, chamando as outras funções em ordem.
@@ -38,7 +54,48 @@ int main() {
     // - Aloca a memória para o mapa do mundo e verifica se a alocação foi bem-sucedida.
     // - Preenche os territórios com seus dados iniciais (tropas, donos, etc.).
     // - Define a cor do jogador e sorteia sua missão secreta.
+    Territorio expansao[TERR_MAX]; // Array do tipo Territorrio que vamos utilizar para manipular cada
+	                               // elemento da struct.
+	
+	printf("=============================== \n");
+	printf("----- Bem-vindo ao jogo War! ----- \n"); // Apresentação do jogo
+	printf("=============================== \n\n");
 
+    printf("Vamos cadastrar os 5 territorios inicias do nosso mundo. \n\n");
+
+    // Aqui iniciamos o for responsável por captar os dados dos 5 territórios.
+	
+	for (int i = 0; i < TERR_MAX; i++) {
+		printf("\n--- Cadastrando Territorio %d --- \n", (i + 1));
+		
+		// Entrada de dados
+		printf("Nome do Territorio:");
+		fgets(expansao[i].nome, sizeof(expansao[i].nome), stdin); 
+		
+		printf("Cor do Exercito (ex: Azul, Verde):");
+		fgets(expansao[i].cor, sizeof(expansao[i].cor), stdin);
+		
+		expansao[i].nome[strcspn(expansao[i].nome, "\n")] = '\0'; // O uso do strcspn() para garantir a remoção
+		expansao[i].cor[strcspn(expansao[i].cor, "\n")] = '\0';  // dos "\n" de cada fgets.
+		
+		printf("Numero de Tropas:");
+		scanf("%d", &expansao[i].tropas); 
+
+        LimparBufferEntrada();  // a função limpar buffer é essencial nesta parte para garantir
+		                        // a remoção do "\n" atribuida pelo scanf.
+    }
+
+    printf("\n =============================== \n");
+    printf("MAPA DO MUNDO - ESTADO ATUAL \n");
+	printf("=============================== \n\n");
+
+    // for responsável pela impressão dos dados.
+	for (int i = 0; i < TERR_MAX; i++){
+	    printf("\nTERRITORIO %d: \n", (i + 1));
+	    printf("  - Nome: %s \n", expansao[i].nome);
+	    printf("  - Dominado por: Exercito %s \n", expansao[i].cor);
+	    printf("  - Tropas: %d \n", expansao[i].tropas);
+	}
     // 2. Laço Principal do Jogo (Game Loop):
     // - Roda em um loop 'do-while' que continua até o jogador sair (opção 0) ou vencer.
     // - A cada iteração, exibe o mapa, a missão e o menu de ações.
